269f1584262f7b021991569b072d1645ff682b91
AladdinHammodi <Aladdin.Hammodi@gmail.com>
2021-01-27 18:46:21 +0200

    push

--- src/public/app.js
+++ src/public/app.js
@@ -1,28 +1,31 @@
-
-const getI = document.getElementById("brands");
-
-fetch("http://localhost:5000/getsmartphone")
+const getI = document.getElementById('brands')
+getI.addEventListener('keyup', event => {
+  fetch('http://localhost:5000/autocomplete', {
+    method: 'POST',
+    body: {
+      word: event.target.value
+    }
+  })
     .then(response => response.json())
     .then(data => {
-        getInput(data)
-    } )
+      getInput(data)
+    })
+})
 
-function getInput(data){
-    const arr = data.data
-    for (let i =0; i<arr.length; i++){
-        const option =document.createElement("option");
-        
-        console.log(arr)
-        option.value=arr[i];
-        option.textContent=arr[i];
-        document.getElementById('brands-smart').appendChild(option);
-        
-    }
+// fetch("http://localhost:5000/getsmartphone")
+//     .then(response => response.json())
+//     .then(data => {
+//         getInput(data)
+//     } )
 
+function getInput (data) {
+  const arr = data.data
+  for (let i = 0; i < arr.length; i++) {
+    const option = document.createElement('option')
 
-
+    console.log(arr)
+    option.value = arr[i]
+    option.textContent = arr[i]
+    document.getElementById('brands-smart').appendChild(option)
+  }
 }
-
-// getI.addEventListener("keyup",event=>{
-    //     data.startsWith(getI.value);
-    // })

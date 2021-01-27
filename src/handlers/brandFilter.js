2d67cb7fd84caa6f0d5353bdda553a74c2607d44
AladdinHammodi <Aladdin.Hammodi@gmail.com>
2021-01-27 13:54:30 +0200

    sunc function

--- src/handlers/brandFilter.js
+++ src/handlers/brandFilter.js
@@ -1,10 +1,36 @@
+const path = require('path')
+const fs = require('fs')
+const filePath = path.join(__dirname, '..', 'data', 'data.json')
+
+// function getAllBrands () {
+//     console.log("getbrands start")
+//     let a = 'aaa'
+//    fs.readFile(filePath, { encoding: 'utf-8' }, (err, file) => {
+//     if (err) {
+//       return { error: true, message: erorr.message }
+//     } else {
+//         a =  brandFilter(JSON.parse(file))
+//       return { error: false, data: brandFilter(JSON.parse(file)) }
+//     }
+//   });
+//   return a
+// }
+function getAllBrands () {
+    try {
+        const data = fs.readFileSync(filePath, { encoding: 'utf-8' })
+        return {error: false, data: brandFilter(JSON.parse(data))}
+    } catch (error) {
+        return {error: true,message: error.message}
+    }
+}
+
 function brandFilter (data) {
   let arr = []
+  const { results } = data
   let nonDuplicated = []
-  for (let index of data) {
+  for (let index of results) {
     //console.log(index)
     arr.push(index.Brand)
-    
   }
 
   for (i = 0; i < arr.length; i++) {
@@ -13,7 +39,6 @@ function brandFilter (data) {
     }
   }
 
-  console.log(nonDuplicated.sort())
-  console.log(nonDuplicated.length)
+  return nonDuplicated
 }
-module.exports = brandFilter
+module.exports = getAllBrands
--- src/handlers/brandHandlers.js
+++ src/handlers/brandHandlers.js
@@ -1,40 +1,22 @@
-// read from data.json 
-const path = require("path")
-const fs = require("fs")
-const brandFilter = require("./brandFilter")
+// read from data.json
+const getAllBrands = require('./brandFilter')
 
 // url.searchparams aftr fetch, use on server also
 //deployment
 // data  filter
 // startswith
 // brand filter function
-// remove duplicates 
+// remove duplicates
 
-
-const filePath = path.join(__dirname,"..", "data", "data.json")
-
-function brandHandlers(req,res){
-    console.log(filePath, "brandhandlers")
-    // read data from ./data/data.json 
-    // filePath declared to get the data.json destination
-    // file => filename
-    fs.readFile(filePath,'utf8',(err,file) => {
-       if (err){
-           console.log(err);
-           res.writeHead(404,{"content-type":"text/html"})
-           res.end("<h1>not found</h1>")
-       }
-       else {
-           res.writeHead(200,{ "contetn-type": "application/json" });
-           //console.log(JSON.parse(file).results[0].Brand, "brand")
-           brandFilter(JSON.parse(file).results)
-        res.end(JSON.parse(file));
-       }
-       
-   });
-   //console.log(item)
-   
-
+function brandHandlers (req, res) {
+  const brandsReturn = getAllBrands()
+  if (brandsReturn.error) {
+    res.writeHead(500, { 'content-type': 'text/html' })
+    res.end('<h1>server error</h1>')
+  } else {
+    res.writeHead(200, { 'content-type': 'apllication/json' })
+    res.end(JSON.stringify(getAllBrands()))
+  }
 }
 
-module.exports =brandHandlers;
+module.exports = brandHandlers
